d85f773a2820152e4e5999dee8cd2a4577365484
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-10-22 16:10:11 +0300

    Added nullable fields

--- Shared/Movies.cs
+++ Shared/Movies.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace blazorsample02.Shared;
 
 public class Movies
 {
+    [Key]
     public int Id { get; set; }
-    public string Movie_Name { get; set; }
-    public string Director { get; set; }
-    public string Year { get; set; }
+    public string? Movie_Name { get; set; }
+    public string? Director { get; set; }
+    public string? Year { get; set; }
 
 }
